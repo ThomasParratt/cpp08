acbd45a0e5653e830b50663d178eb53eac11020a
ThomasParratt <thomasparratt@gmail.com>
2025-02-07 11:34:19 +0200

    stated MutantStack member functions

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack
+{
+    private:
+
+    public:
+        bool            empty();
+        //unsigned int    size();
+};
