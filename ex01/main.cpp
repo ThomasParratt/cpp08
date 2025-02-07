c92821f7b09b42af95e3b8ba1979874c1dadc27b
ThomasParratt <thomasparratt@gmail.com>
2025-02-07 14:45:18 +0200

    updated main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,13 +3,28 @@
 int main(void)
 {
     Span sp = Span(3);
-    sp.addNumber(2);
-    sp.addNumber(10);
-    sp.addNumber(5);
-    std::cout << "Shortest span: " << sp.shortestSpan() << std::endl; // try catch here?
-    std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+    try
+    {
+        sp.addNumber(2);
+        sp.addNumber(10);
+        sp.addNumber(5);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    
 
-    Span sp1 = Span(2);
+    Span sp1 = Span(5);
     std::vector<int> temp = {1, 2, 3, 4, 5};
     try
     {
@@ -21,7 +36,7 @@ int main(void)
     }
     try
     {
-        std::cout << "Shortest span: " << sp1.shortestSpan() << std::endl; // can these both be in a try statement?
+        std::cout << "Shortest span: " << sp1.shortestSpan() << std::endl;
         std::cout << "Longest span: " << sp1.longestSpan() << std::endl;
     }
     catch(const std::exception& e)
